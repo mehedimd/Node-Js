2df5ed62369651d622cfce89d076d300c491d81c
mehedimd <mehedi105662@gmail.com>
2024-02-18 10:54:56 +0600

    add Local Module

---
+++ Local Modules/export.js
@@ -0,0 +1,13 @@
+const getName = () => {
+  return "Mehedi Hasan Rasel";
+};
+
+exports.getRoll = () => 1280692;
+
+// exports.getName = getName;
+// exports.getRoll = getRoll;
+
+// module.exports = {
+//   getName,
+//   getRoll,
+// };
